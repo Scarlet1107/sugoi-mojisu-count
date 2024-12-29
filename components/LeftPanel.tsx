1eeb1fe8040ba38000b7511148c4fa7886753b48
Scarlet1107 <shougo.ushiro@gmail.com>
2024-12-29 21:40:31 +0900

    add local storage

--- components/LeftPanel.tsx
+++ components/LeftPanel.tsx
@@ -14,7 +14,6 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { Button } from "./ui/button";
 import CopyButton from "./CopyButton";
 
 const LeftPanel = () => {
@@ -24,38 +23,35 @@ const LeftPanel = () => {
   const [currentCount, setCurrentCount] = useState<number>(0); // 文字数カウント
   const [goalCount, setGoalCount] = useState<number>(200); // 目標文字数
 
-  // text, includeSpaces, includeLineBreaks の状態が変わるたびに自動で文字数を再計算
   useEffect(() => {
-    let processedText = text;
+    const savedText = localStorage.getItem("text");
+    if (savedText) {
+      setText(savedText);
+    }
+  }, []);
 
-    // 空白を除外する場合
+  useEffect(() => {
+    // textが変更されるたびにlocalStorageに保存
+    localStorage.setItem("text", text);
+    let processedText = text;
     if (!includeSpaces) {
-      // \s には空白文字やタブ、改行などすべて含まれるため、
-      // 改行の除外/含有設定を別でやりたい場合は注意が必要
       processedText = processedText.replace(/\s/g, "");
     }
-
-    // 改行を除外する場合
     if (!includeLineBreaks) {
-      // 改行を除外するだけならこちら
-      // ただし \s の中にも \n は含まれるため、上の処理との使い分け注意
       processedText = processedText.replace(/\n/g, "");
     }
-
-    // 日本語など全角文字を考慮してサロゲートペアを正しく数えるため、スプレッド構文で配列化してlengthを取る
     const length = [...processedText].length;
     setCurrentCount(length);
   }, [text, includeSpaces, includeLineBreaks]);
 
   const handleTextChange = (newText: string) => {
-    setText(newText); // テキストを更新
+    setText(newText);
   };
 
   return (
     <div className="w-full h-full pt-8 pl-8 pr-4">
       <div className="flex justify-between items-center">
         <div className="flex space-x-8">
-          {/* 「現在」の表示を text.length から count に変更 */}
           <div className="flex items-center">
             <p>現在： </p>
             <p>{currentCount}</p>
@@ -103,8 +99,6 @@ const LeftPanel = () => {
 
       <div className="flex space-x-8 items-centers mt-4">
         <div className="flex items-center space-x-1">
-          {/* チェックボックスに checked と onCheckedChange を設定し、
-              includeSpaces と連動させる */}
           <Checkbox
             id="space"
             checked={includeSpaces}
@@ -115,7 +109,6 @@ const LeftPanel = () => {
           <Label htmlFor="space">空白を含める</Label>
         </div>
         <div className="flex items-center space-x-1">
-          {/* 同様に、改行を含める設定も連動させる */}
           <Checkbox
             id="lineBreak"
             checked={includeLineBreaks}
