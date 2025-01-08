9c3ccbd1ec7ce108015da56e0cc1ad32a7621e2e
Scarlet1107 <shougo.ushiro@gmail.com>
2025-01-08 19:52:19 +0900

    remove image configuration from next.js config

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   basePath: "/word-counter",
   assetPrefix: "/word-counter/",
-  images: {
-    path: "/word-counter/_next/image",
-    loader: "default",
-  },
 };
 
 export default nextConfig;
