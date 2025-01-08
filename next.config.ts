8e3f26af2afbc18dcc1dfd1e95b18091994c9b8b
Scarlet1107 <shougo.ushiro@gmail.com>
2025-01-08 19:35:41 +0900

    add assetPrefix and image configuration for word counter

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: "/word-counter",
+  assetPrefix: "/word-counter/",
+  images: {
+    path: "/word-counter/_next/image",
+    loader: "default",
+  },
 };
 
 export default nextConfig;
