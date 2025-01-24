e3c04d1d7221a5bcb7a406b358660981f546506e
Scarlet1107 <shougo.ushiro@gmail.com>
2025-01-24 23:37:09 +0900

    set metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,10 +14,27 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://scarlet7.net"),
-  title: "My webpage",
-  description: "This is my webpage",
+  title: "Scarlet",
+  description:
+    "WebエンジニアのScarletです。開発したアプリをまとめています。ご自由にお使いください。お仕事の依頼はこちらからscarlet7.second@gmail.com",
   icons: {
     icon: "https://scarlet7.net/icon.png",
+  },
+  openGraph: {
+    type: "website",
+    locale: "ja_JP",
+    url: "https://scarlet7.net",
+    title: "Scarlet",
+    description:
+      "WebエンジニアのScarletです。開発したアプリをまとめています。ご自由にお使いください。今まで作成したアプリ一覧、「すごい文字数カウント」「タイヤ見積もりアプリ」など。お仕事の依頼はこちらからscarlet7.second@gmail.com",
+    images: [
+      {
+        url: "https://scarlet7.net/icon.png",
+        width: 800,
+        height: 600,
+        alt: "Scarlet",
+      },
+    ],
   },
 };
 
